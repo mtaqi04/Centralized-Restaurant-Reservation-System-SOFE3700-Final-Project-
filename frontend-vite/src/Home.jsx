9e7bf97e2e77838c847c226ea5828d80c2bbd175
LilMactop17 <mohamed.tawfikomar@ontariotechu.net>
2025-11-14 17:12:41 -0500

    added routing

---
+++ frontend-vite/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const { useState, useEffect } = React;
+
+
+function Home() {
+    return (
+        <>
+            <h1>Home Page</h1>
+        </>
+    );
+}
+
+export default Home;
