9cf859564181f942acf8356790bf1920ef8f4d0b
LilMactop17 <mohamed.tawfikomar@ontariotechu.net>
2025-11-13 19:59:22 -0500

    Revert "added fs"
    This reverts commit 4934d76c7232083ca8bfc602c645bc1c4f393114.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ import { getWeatherForLocation, insertWeatherDB } from "./services/weatherServic
 import reservationRoutes from './routes/reservationRoutes.js';
 import authRoutes from "./routes/authRoutes.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 
